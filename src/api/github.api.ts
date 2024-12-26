f5b462123238758c8ec22828184bc48a6bc67fde
4ndresdev <4ndresdev@gmail.com>
2024-12-26 16:43:49 -0600

    Add .env.template and update README; ignore .env file in git

--- src/api/github.api.ts
+++ src/api/github.api.ts
@@ -4,5 +4,6 @@ export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
   },
 });
